87247b2e5d1f57827aa34516a244855d65b71598
Tushar Jain <tusharjain0022@gmail.com>
2020-11-06 17:59:27 +0530

    module 2 assignment submission

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from "./ContactComponent";
 import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -60,6 +61,10 @@ class Main extends Component {
           />
           <Route path="/menu/:dishId" component={WithDishId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
