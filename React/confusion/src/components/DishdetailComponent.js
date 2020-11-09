6d4677737827232d250d6d58017e611b8551f1ee
Tushar Jain <tusharjain0022@gmail.com>
2020-11-09 21:11:26 +0530

    Redux thunk added

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import {
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { required, minLength, maxLength } from "./ContactComponent";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 function RenderDish({ dish }) {
   if (dish != null)
@@ -151,30 +152,48 @@ function CommentForm({ addComment, dishId }) {
   );
 }
 const DishDetail = (props) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
         </div>
       </div>
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish dish={props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <RenderComment comments={props.comments} />
-          <CommentForm addComment={props.addComment} dishId={props.dish.id} />
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else if (props.dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComment comments={props.comments} />
+            <CommentForm addComment={props.addComment} dishId={props.dish.id} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 export default DishDetail;
--- React/confusion/src/redux/configureStore.js
+++ React/confusion/src/redux/configureStore.js
@@ -1,17 +1,20 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Dishes } from "./dishes";
 import { Comments } from "./comments";
-import { Promotion } from "./promotion";
+import { Promotions } from "./promotion";
 import { Leaders } from "./leaders";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const ConfigureStore = () => {
   const store = createStore(
     combineReducers({
       dishes: Dishes,
       comments: Comments,
-      promotions: Promotion,
+      promotions: Promotions,
       leaders: Leaders,
-    })
+    }),
+    applyMiddleware(thunk, logger)
   );
 
   return store;
